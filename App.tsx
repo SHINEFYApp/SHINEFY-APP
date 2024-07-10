faef9a711425ed027cf4d09843ad5acb0718112d
Mahmoud Khalil <99986188+midokhalil1987@users.noreply.github.com>
2024-07-10 20:38:12 +0300

    Update home save location page and fix text display in android

--- App.tsx
+++ App.tsx
@@ -3,9 +3,14 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppProvider, AppConsumer} from './src/Provider/context/AppProvider';
 import Stacknav from './src/Provider/Routenavigation';
-import {I18nManager} from 'react-native';
+import {I18nManager, Platform} from 'react-native';
 import RNRestart from 'react-native-restart';
 import {localStorage} from './src/Provider/utilslib/Utils';
+import enableFontPatch from './src/Provider/utilslib/enableFontPatch.js';
+
+if (Platform.OS === 'android') {
+  enableFontPatch();
+}
 
 global.MapAddress = 'NA';
 global.pushRedirection = 0;
@@ -20,8 +25,8 @@ class App extends Component {
       'languagesetenglish',
     );
 
-    if (languagecathc == 0) {
-      if (languagesetenglish == 3) {
+    if (languagecathc === 0) {
+      if (languagesetenglish === 3) {
         if (I18nManager.isRTL) {
           I18nManager.forceRTL(true);
           I18nManager.allowRTL(true);
